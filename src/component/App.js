56644be9b35787c178da7fc276cf1ceaf4672578
Ryan Bateman <lesserchance@gmail.com>
2020-01-11 13:45:12 -0500

    move to proper react component

--- src/component/App.js
+++ src/component/App.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import './app.css';
 
-function App() {
-  return (
-    <div className="App">
-      <div className="jumbotron text-center">
-        <img src="./assets/logo.png" alt="My App" width="150" />
-        <h2>My App</h2>
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div className="jumbotron text-center">
+          <img src="./assets/logo.png" alt="My App" width="150" />
+          <h2>My App</h2>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
